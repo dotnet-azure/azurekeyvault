c80a132a8d5c250448ad9fba3e69f45d6a32e256
mmohsene <mohamed.mohsen-elsayed@valeo.com>
2024-05-07 02:37:24 +0300

    initial commit.

---
+++ Controllers/VaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AzureKeyVault.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VaultController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public VaultController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    [Route("secret")]
+    public string? GetSecret(string secretName)
+    {
+        return _configuration[secretName];
+    }
+}
